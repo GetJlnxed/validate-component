bfefec1f8dafbb746be537dcebc35fb163ce83fa
rromanovsky <r.romanovsky@antraks.ru>
2021-02-11 12:07:00 +0300

    added validate-component, removed trash

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <validate-component num="1">
+      <div>
+        <validate-component num="2"></validate-component>
+      </div>
+      <div>
+        <validate-component num="3">
+          <validate-component num="4" />
+        </validate-component>
+      </div>
+      <validate-component num="5"></validate-component>
+    </validate-component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ValidateComponent from "./components/ValidateComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ValidateComponent,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ValidateComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="display">
+      <button @click="validate">Validate</button>
+      <p>---{{ num }}</p>
+    </div>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ValidateComponent",
+  props: {
+    num: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    validate() {
+      console.log(`validating - ${this.num}`);
+      this.$children.forEach((child) => {
+        child.validate();
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.display {
+  display: flex;
+  flex-flow: row nowrap;
+}
+</style>
